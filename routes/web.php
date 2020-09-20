2e7292a7b7296f77994f59274f46db05b3e1b643
Alfredo Marenco <marencoalf>
2020-09-20 15:50:44 -0500

    Boton de whatsapp con funcionalidad para mandar el primer mensaje de seguimiento

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\DesarrollosController;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\TextUI\XmlConfiguration\Group;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('dashboard');
 })->middleware('auth');
@@ -46,7 +35,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/user/profile', function () {
-    return view('dashboard');
+    return view('profile.show');
 })->name('user.profile');
 
 Route::get('/email/verify', function () {
